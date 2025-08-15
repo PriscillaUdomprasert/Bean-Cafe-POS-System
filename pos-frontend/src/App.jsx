dd5c8a5536f7b97230106865cde8a69e8c11fd6a
PriscillaUdomprasert <udomprasertpp@gmail.com>
2025-08-15 17:11:08 -0500

    Add files

---
+++ pos-frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App
---
+++ pos-frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './Home.jsx'
+export { default as Orders } from './Orders.jsx'
+export { default as Auth } from './Auth.jsx'
+export { default as About } from './About.jsx'
+export { default as Contact } from './Contact.jsx'
