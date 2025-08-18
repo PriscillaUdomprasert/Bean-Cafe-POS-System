09d94866864372d395d76140c5e9ce844c968ec9
PriscillaUdomprasert <udomprasertpp@gmail.com>
2025-08-18 14:29:29 -0500

    WIP

---
+++ pos-frontend_v3/app/routes/auth/login.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
--- pos-frontend_v3/app/routes/root/home.tsx
+++ pos-frontend_v3/app/routes/root/home.tsx
@@ -23,7 +23,7 @@ const HomePage = () => {
     
         
     
-  )
-}
+  );
+};
 
 export default HomePage
